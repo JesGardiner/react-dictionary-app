4c7676301e5636d1b2d2699ae7ac01ee274100b5
JesGardiner <jes.gardiner21@gmail.com>
2024-08-21 10:57:52 +0100

    Applied finishing styling touches to the project

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          ENGLISH DICTIONARY
+          English Dictionary
         </header>
         <main>
           <Dictionary />
