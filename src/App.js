e0283cbc953d1157ba214338b8cacfd5bdf0160d
JesGardiner <jes.gardiner21@gmail.com>
2024-08-23 10:59:35 +0100

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,27 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          English Dictionary
-        </header>
+        <header className="App-header">English Dictionary</header>
         <main>
           <Dictionary />
         </main>
         <footer className="App-footer">
-          <small>Coded by Jess Gardiner, open sourced on Github & hosted on Netlify </small>
+          <small>
+            Coded by Jess Gardiner, open sourced on{" "}
+            <a
+              href="https://github.com/JesGardiner target=_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>{" "}
+            & hosted on{" "}
+            <a
+              href="https://rainbow-narwhal-53fa5b.netlify.app/ target=_blank"
+              rel="noreferrer"
+            >
+              Netlify
+            </a>{" "}
+          </small>
         </footer>
       </div>
     </div>
